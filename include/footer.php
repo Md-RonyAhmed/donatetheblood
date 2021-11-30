f290e0f0266e7be7acd09d26dc9fd34ac4119a11
Md. Rony <rony15-10527@diu.edu.bd>
2021-11-30 14:50:48 +0600

    This is my university project called blood donation website

---
+++ include/footer.php
@@ -0,0 +1,35 @@
+<!-- contact us section -->
+<div class="container-fluid">
+	<div class="row contactus">
+		<div class="col-md-6 offset-md-3">
+			<h1 class="text-center">Contact Us</h1>
+			<hr class="white-bar">
+
+			<h1 class="text-center">
+				<a target="_blank" href="https://www.facebook.com/https://www.facebook.com/profile.php?id=100007569846753"><i style="color: #fff;" class="fab fa-facebook-square"></i></a>
+
+				<a target="_blank" href="https://www.youtube.com/https://www.youtube.com/channel/UCMptxqLqiu1b-Jgxdhjo5Lw/featured?disable_polymer=true"><i style="color: #fff;" class="fab fa-youtube-square"></i></a>
+			</h1>
+
+			<div class="details">
+				<p> 01792395386 <i class="fa fa-phone"></i>
+					ryasin420@gmail.com <i class="fa fa-envelope"></i> </p>
+			</div>
+
+		</div>
+	</div>
+</div>
+<!-- end contact us section -->
+
+<!-- JQuery File -->
+<script type="text/javascript" src="jquery/jquery.js"></script>
+
+<!-- BootStrap JS File-->
+<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+
+<!-- Fontawesome Icon JS-->
+<script defer src="fontawesome/js/all.js"></script>
+
+</body>
+
+</html>
